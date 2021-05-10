452d728c894ba6610df6160e8c031d8d52a26336
nayoon030303 <nayoon030303@gmail.com>
2021-05-11 08:15:47 +0900

    Events and event handlers

--- vanil.js/chap3/index.js
+++ vanil.js/chap3/index.js
@@ -1,7 +1,14 @@
-//document also object
+
+/**
+ * document also object
+ * DOM : Document Object Module
+ * js는 html에 있는 모든 요소를 가져오고 그리고 객체로 변환한다
+ *
+ * 내 패이지에서 자바스크립틀 선택한 거는 객체가 된다.
+ */
+
+
 //console.log(document);
-
-
 //const title = document.getElementById("title");
 
 //querySelector는 노드의 첫번째 자식을 반환
@@ -13,9 +20,21 @@ document.title = "I own you now";
 
 console.dir(title);
 
-/**
- * DOM : Document Object Module
- * js는 html에 있는 모든 요소를 가져오고 그리고 객체로 변환한다
- *
- * 내 패이지에서 자바스크립틀 선택한 거는 객체가 된다.
+
+/*
+ * Events and event handlers
+ * js는 이벤트에 반응하기 위해 만들어졌다
+ * 이벤트: 웹사이트에서 발생하는 것
  */
+
+function handleResize(event){
+    console.log(event);
+
+}
+
+// window.addEventListener("resize",handleResize());// handelResize() 지금 바로 호출
+//window.addEventListener("resize",handleResize); //handleResize 함수를 호출, 내가 필요한 시점에
+function handleClick(){
+    title.style.color =  "blue";
+}
+title.addEventListener("click",handleClick);
