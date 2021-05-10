ceaa5bf387a0445232f3c186589a1bc8b37de66d
nayoon030303 <nayoon030303@gmail.com>
2021-05-10 23:53:02 +0900

    DOM Functions

---
+++ vanil.js/chap3/index.js
@@ -0,0 +1,15 @@
+//document also object
+//console.log(document);
+
+const title = document.getElementById("title");
+
+console.log(title);
+title.innerHTML = "HI! From JS";
+
+
+/**
+ * DOM : Document Object Module
+ * js는 html에 있는 모든 요소를 가져오고 그리고 객체로 변환한다
+ *
+ * 내 패이지에서 자바스크립틀 선택한 거는 객체가 된다.
+ */
