6420d37c61ca743b1ce4a8e7ee2815094e4a9933
nayoon030303 <nayoon030303@gmail.com>
2021-05-11 13:23:47 +0900

    DOM If else Function practice part Two_toggle

--- vanil.js/chap5/index.js
+++ vanil.js/chap5/index.js
@@ -4,11 +4,7 @@ const CLICKED_CLASS = "clicked";
 
 function handleClick(){
     const hasClass = title.classList.contains(CLICKED_CLASS);
-    if(hasClass){
-        title.classList.remove(CLICKED_CLASS);
-    }else{
-        title.classList.add(CLICKED_CLASS);
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init(){
