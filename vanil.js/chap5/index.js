394aef9322954e33589e894170faa7912317f27e
nayoon030303 <nayoon030303@gmail.com>
2021-05-11 12:22:01 +0900

    DOM If else Function practice part Two_classList

--- vanil.js/chap5/index.js
+++ vanil.js/chap5/index.js
@@ -3,11 +3,11 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-    const currentClass = title.className;
-    if(currentClass !== CLICKED_CLASS){
-        title.className = CLICKED_CLASS;
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+    if(hasClass){
+        title.classList.remove(CLICKED_CLASS);
     }else{
-        title.className = "";
+        title.classList.add(CLICKED_CLASS);
     }
 }
 
